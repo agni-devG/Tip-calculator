54d6041282757c8de12f6bf2fc2da78692e1cea0
Akshat <Akshatdx12345@gmail.com>
2021-12-06 00:40:44 +0530

    js completed and commited

--- app.js
+++ app.js
@@ -4,34 +4,67 @@ const howMany = document.querySelector('.howMany');
 const dispTip = document.querySelector('#tip-amount'); 
 const dispTotal = document.querySelector('#total') ;
 const btns = document.querySelectorAll('.btn')
+const resetBtn = document.querySelector('.reset-btn')
+const cPercent = document.querySelector('.custom')
 
+// VARIABLES
 let bill = 0;
 let perCent = 0;
 let personsNo = 0;
-billAmt.addEventListener('input',function(){
-    bill = billAmt.valueAsNumber;
-})
 
+// FUNCTIONS
+let reset = () => {
+    bill = 0;
+    perCent = 0;
+    personsNo = 0;
+    billAmt.value = ''
+    howMany.value = ''
+    cPercent.value = ''
+    dispTotal.innerText = '$0.00';
+    dispTip.innerText = '$0.00';
+}
 
+let calculate = () =>  {
+    if (perCent !== 0 ){
+        const tipTotal = (perCent*bill)/100;
+        const tipPerPerson = tipTotal/personsNo;
+        if (personsNo <= 0 ){
+            dispTotal.innerText = '$0.00';
+            dispTip.innerText =  '$0.00';
+        }
+        else{
+            dispTotal.innerText = `$${Math.round(tipTotal*100)/100}`;
+            dispTip.innerText =  `$${Math.round(tipPerPerson*100)/100}`;
+        }
+    }
+}
+
+// EVENT LISTENERS
 for(let btn  of btns){
     btn.addEventListener('click',function(){
         perCent = parseInt(btn.innerText);
         calculate();
     })
 }
+
+billAmt.addEventListener('input',function(){
+    bill = billAmt.valueAsNumber;
+    calculate();
+})
+
 howMany.addEventListener('input',function(){
-    personsNo = howMany.valueAsNumber
+    personsNo = howMany.value;
+    calculate();
 })
 
 percentForm.addEventListener('submit',function(e){
     e.preventDefault();
 });
+resetBtn.addEventListener('click',reset);
+cPercent.addEventListener('input',function(){
+    perCent = cPercent.value;
+    calculate();
+})
 
 
 
-let calculate = function (){
-    const tipTotal = (perCent*100)/bill;
-    const tipPerPerson = tipTotal/personsNo;
-    dispTotal.innerText = tipTotal;
-    dispTip.innerText =  tipPerPerson
-}
